3e4fe11469fcfac20161551d14bbfbc35aa8799a
Pascal Goldbrunner <modac@gmx.net>
2021-03-25 12:49:16 +0100

    Added avr arch check

--- include/NewAttinyOneWire.h
+++ include/NewAttinyOneWire.h
@@ -1,6 +1,10 @@
 #ifndef NewAttinyOneWire_h
 #define NewAttinyOneWire_h
 
+#if (__AVR_ARCH__ < 100)
+  #error "This library is designed for 'modern AVR' parts only (post-2016, when they revised peripherals and instruction set timing)"
+#endif
+
 class NewAttinyOneWire {
     public:
         static void initOneWireUART();
