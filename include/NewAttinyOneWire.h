7264e79ebf1293b8552f51c275e869fa9d43ec91
Pascal Goldbrunner <modac@gmx.net>
2021-03-25 12:53:23 +0100

    Added TODO

--- include/NewAttinyOneWire.h
+++ include/NewAttinyOneWire.h
@@ -1,6 +1,7 @@
 #ifndef NewAttinyOneWire_h
 #define NewAttinyOneWire_h
 
+// TODO: maybe only allow avrxmega3 devices (103)
 #if (__AVR_ARCH__ < 100)
   #error "This library is designed for 'modern AVR' parts only (post-2016, when they revised peripherals and instruction set timing)"
 #endif
